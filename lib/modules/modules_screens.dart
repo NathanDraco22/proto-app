e03c926e5a00da27dfa1499e716791e02965aee8
NathanDraco22 <nathandraco22@gmail.com>
2023-10-08 17:35:19 -0600

    barrel file added

---
+++ lib/modules/modules_screens.dart
@@ -0,0 +1,2 @@
+export 'package:proto_app/modules/coin_cap_tracker/view/coin_screen.dart';
+export 'package:proto_app/modules/home/home_screen.dart';
--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,7 +1,5 @@
 import 'package:go_router/go_router.dart';
-import 'package:proto_app/modules/coin_cap_tracker/view/coin_screen.dart';
-
-import 'package:proto_app/modules/home/home_screen.dart';
+import 'package:proto_app/modules/modules_screens.dart';
 
 class AppRouter {
 
